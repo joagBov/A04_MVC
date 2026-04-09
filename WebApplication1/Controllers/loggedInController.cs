c9dc3b12f1ae5ccef03383ff2bbd955bb37b9006
Elijah Atta-Konadu <Eattakonadu2660@conestogac.on.ca>
2026-04-09 15:15:37 -0400

    Created a log in controller and a landing page for logged in/registered  users

---
+++ WebApplication1/Controllers/loggedInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class loggedInController : Controller
+    {
+        public IActionResult authorized()
+        {
+            return View();
+        }
+    }
+}
