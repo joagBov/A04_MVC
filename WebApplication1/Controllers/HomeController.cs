d337c5afd9437e879b7f88ea5c7435206f837d31
Elijah Atta-Konadu <Eattakonadu2660@conestogac.on.ca>
2026-04-09 15:58:14 -0400

    Stored the users info into a list when registering

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static List<UserAccount> userAccounts = new List<UserAccount>();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -24,6 +25,17 @@ namespace WebApplication1.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserAccount userAccount)
+        {
+            return View(userAccount);
+            if (ModelState.IsValid)
+            {
+                userAccounts.Add(userAccount);
+                return RedirectToAction("authorized", "loggedIn");
+            }
+            return View(userAccount);
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ WebApplication1/Models/UserAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserAccount
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+    }
+}
